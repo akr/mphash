843a725f25d12c5a3045b7d45e77e5762d205fbd
Tanaka Akira <akr@fsij.org>
2008-02-03 02:15:12 +0000

    add test.
    git-svn-id: svn://svn.a-k-r.org/akr/mphash/trunk@799 0ecacc03-ac1b-0410-941d-e442c79421a6

---
+++ test/test-mphf.rb
@@ -0,0 +1,46 @@
+require 'test/unit'
+require 'mphash'
+
+class TestMPHF < Test::Unit::TestCase
+  def check_mphf(keys)
+    mphf = MPHash::MPHF.new(keys)
+    check = {}
+    keys.each {|key|
+      hash = mphf.mphf(key)
+      assert_operator(0, :<=, hash)
+      assert_operator(hash, :<, keys.length)
+      assert_nil(check[hash], "collision: #{hash} : #{check[hash].inspect} #{key.inspect}")
+      check[hash] = key
+    }
+    10.times {
+      key = rand.to_s
+      hash = mphf.mphf(key)
+      if hash != -1
+        assert_operator(0, :<=, hash)
+        assert_operator(hash, :<, keys.length)
+      end
+    }
+  end
+
+  def test_smallset
+    keys = %w[foo bar baz]
+    check_mphf(keys)
+  end
+
+  def test_largeset
+    keys = ["a"]
+    1000.times { keys << keys.last.succ }
+    check_mphf(keys)
+  end
+
+  def test_emptyset
+    keys = []
+    check_mphf(keys)
+  end
+
+  def test_singleton
+    keys = ["a"]
+    check_mphf(keys)
+  end
+
+end
